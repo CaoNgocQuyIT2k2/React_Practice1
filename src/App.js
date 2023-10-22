5b6e2a59d8b8133c06e2717a2f9d0b453472f80d
CaoNgocQuyIT2k2 <caongocquyit2k2@gmail.com>
2023-10-22 13:34:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import './css/index.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Header from './Layout/Header';
+import Banner from './Layout/Banner';
+import Item from './Layout/Item';
+import Footer from './Layout/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className="CustomContainer">
+      <Header />
+      <Banner />
+      <Item/>
+      </div>
+      <Footer />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Layout/Item.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+export default class Item extends Component {
+  render() {
+    return (
+      <section className="pt-4 item">
+        <div className="container px-">
+          <div className="row gx-">
+            <div className="col-lg-4 col-xxl-4 mb-5">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center p-4 p- pt-0 pt-lg-0">
+                  <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i className="bi bi-layout-text-window-reverse" /></div>
+                  <h2 className="fs-4 fw-bold">Fresh new layout</h2>
+                  <p className="mb-0">With Bootstrap 5, we've created a fresh <br /> new layout for this template!</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-xxl-4 mb-5">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center p-4 p- pt-0 pt-lg-0">
+                  <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i className="bi bi-cloud-download" /></div>
+                  <h2 className="fs-4 fw-bold">Free to download</h2>
+                  <p className="mb-0">As always, Start Bootstrap has a powerful <br /> collection of free templates.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-xxl-4 mb-5">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center p-4 p- pt-0 pt-lg-0">
+                  <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i className="bi bi-card-heading" /></div>
+                  <h2 className="fs-4 fw-bold">Jumbotron hero header</h2>
+                  <p className="mb-0">The heroic part of this template is <br /> the jumbotron hero header!</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-xxl-4 mb-5">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center p-4 p- pt-0 pt-lg-0">
+                  <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i className="bi bi-layout-text-window-reverse" /></div>
+                  <h2 className="fs-4 fw-bold">Fresh new layout</h2>
+                  <p className="mb-0">With Bootstrap 5, we've created a fresh <br /> new layout for this template!</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-xxl-4 mb-5">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center p-4 p- pt-0 pt-lg-0">
+                  <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i className="bi bi-cloud-download" /></div>
+                  <h2 className="fs-4 fw-bold">Free to download</h2>
+                  <p className="mb-0">As always, Start Bootstrap has a powerful <br /> collection of free templates.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-xxl-4 mb-5">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center p-4 p- pt-0 pt-lg-0">
+                  <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i className="bi bi-card-heading" /></div>
+                  <h2 className="fs-4 fw-bold">Jumbotron hero header</h2>
+                  <p className="mb-0">The heroic part of this template is <br />the jumbotron hero header!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
